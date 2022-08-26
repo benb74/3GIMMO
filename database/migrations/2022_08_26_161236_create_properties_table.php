4809a7132ebe3a293f1916815bc54bfc4accffc8
Benoit Bequignon <bbequignon@gmail.com>
2022-08-26 18:40:27 +0200

    seller and properties db tables

---
+++ database/migrations/2022_08_26_161236_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('ref_annonce');
+            $table->float('prix_annonce', 10, 2);
+            $table->float('surface_habitable', 6, 2);
+            $table->integer('nombre_de_piece');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+};
