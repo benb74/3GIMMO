3763925aff967248d208439dfc0f612aca2d36a7
Benoit Bequignon <bbequignon@gmail.com>
2022-08-27 15:45:21 +0200

    Eloquent request for properties display

--- app/Http/Controllers/Catalog/MainController.php
+++ app/Http/Controllers/Catalog/MainController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Catalog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('catalog.index');
+        $properties = Property::all();
+
+        return view('catalog.index', compact('properties'));
     }
 }
